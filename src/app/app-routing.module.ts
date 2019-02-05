7397bdaddb752ed1627da3425a1f3d330325d17b
pavithra <pavitra.a@exzatechconsulting.com>
2019-02-05 18:26:58 +0530

    search field added in dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './module/dashboard/dashboard/dashboard.component';
 import { RegistationComponent } from './module/registration/registation.component';
 import { PaymentComponent } from './module/payment/payment.component';
+import { RedirectComponent } from './module/dashboard/redirect/redirect.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'adm-registation', component: RegistationComponent },
-  { path: 'adm-payment', component: PaymentComponent }
+  { path: 'adm-payment', component: PaymentComponent },
+  { path: 'redirect', component: RedirectComponent }
+ 
 ];
 
 @NgModule({
